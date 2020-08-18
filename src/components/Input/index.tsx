23c8dc892ab7818d3a886b7c6627fec1834c442d
Daniel <daniel.ahavat@gmail.com>
2020-08-18 14:19:38 -0300

    input validation error

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -46,6 +46,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         ref={inputRef}
         {...rest}
       />
+      {error}
     </Container>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,10 +2,12 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-useless-path-segments */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
-import * as yup from 'yup';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErros from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -15,23 +17,32 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUP: React.FC = () => {
-  function handleSubmit(data: object): void {
-    try {
-      const schema = Yup.object().shape({
-        name: yup.string().required('Nome é obrigatório'),
-        email: yup.string().required('Email Obrigatporio').email('Digite um email válido'),
-        password: yup.string().required('Senha Obrigatória').min(&, 'No mínimo 6 digitos'),
-      })
-    } catch (err) {
+  const formRef = useRef<FormHandles>(null);
 
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string()
+          .required('Email Obrigatporio')
+          .email('Digite um email válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErros(err);
+      formRef.current?.setErrors(errors);
     }
-  }
+  }, []);
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="Email" />
