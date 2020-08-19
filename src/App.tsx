940bededb1c45787e49a243e40877ab40b8a5566
Daniel <daniel.ahavat@gmail.com>
2020-08-19 00:04:00 -0300

    Styled component error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable import/extensions */
 import React from 'react';
 import GlobalStyle from './styles/global';
-// import SignIn from './pages/Signin/index';
-import SignUP from './pages/SignUp/index';
+// import SignUP from './pages/SignUp/index';
+import SignIn from './pages/Signin';
 
 const App: React.FC = () => (
   <>
-    <SignUP />
+    <SignIn />
     <GlobalStyle />
   </>
 );
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-useless-path-segments */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
 
+import getValidationErros from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -11,29 +15,51 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form>
-        <h1>Faça seu Logon</h1>
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
-        <Button type="submit">Entrar</Button>
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
-      <a href="">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-    <Background />
-  </Container>
-);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email Obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().min(6, 'Senha obrigatória'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErros(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu Logon</h1>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+        <a href="">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 export default SignIn;
